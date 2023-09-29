b413203b6a0e00c4351e7c298c3c93d2a4c1b1e9
Yuri Mendes <101746093+yurialmeida24@users.noreply.github.com>
2023-09-29 15:37:01 -0300

    Delete OneDrive/Documentos/ITA yuri/2023/ces26/atividade 4/meuProjeto directory

--- OneDrive/Documentos/ITA yuri/2023/ces26/atividade 4/meuProjeto/public/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('botao').addEventListener('click', function () {
-  fetch('/dados')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('paragrafo').textContent = data.mensagem;
-    })
-    .catch(error => console.error('Erro ao buscar dados:', error));
-});
